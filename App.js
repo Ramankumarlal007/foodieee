1047f55989c618723bef0e15ccc4ca8c1565014b
Raman Kumar <ramankumarlal@outlook.com>
2024-01-28 09:55:56 +0530

    restaurant data

--- App.js
+++ App.js
@@ -66,36 +66,548 @@ const header = () => {
     </>
   );
 };
+
+let restaurants = [
+  {
+    "info": {
+    "id": "452627",
+    "name": "Chaayos Chai+Snacks=Relax",
+    "cloudinaryImageId": "cace805a6ba74137571d0f7ac92302b1",
+    "locality": "RMS Layout",
+    "areaName": "Sahakara Nagar",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Bakery",
+    "Beverages",
+    "Chaat",
+    "Desserts",
+    "Home Food",
+    "Italian",
+    "Maharashtrian",
+    "Snacks",
+    "Street Food",
+    "Sweets"
+    ],
+    "avgRating": 4.4,
+    "parentId": "281782",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 18,
+    "lastMileTravel": 1.9,
+    "serviceability": "SERVICEABLE",
+    "slaString": "15-20 mins",
+    "lastMileTravelString": "1.9 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-01-29 02:00:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {},
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "64754",
+    "name": "A2B - Adyar Ananda Bhavan",
+    "cloudinaryImageId": "pjg4r1tolb7hqi5eecgs",
+    "locality": "Bhadrappa Layout",
+    "areaName": "Hebbal",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "South Indian",
+    "North Indian",
+    "Sweets",
+    "Chinese"
+    ],
+    "avgRating": 4.5,
+    "veg": true,
+    "parentId": "22",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "10K+",
+    "sla": {
+    "deliveryTime": 26,
+    "lastMileTravel": 3.9,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "3.9 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-01-28 22:30:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹125 OFF",
+    "subHeader": "ABOVE ₹199",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/a2b-adyar-ananda-bhavan-bhadrappa-layout-hebbal-bangalore-64754",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "757051",
+    "name": "Theobroma",
+    "cloudinaryImageId": "b033728dcb0101ceb19bff0e1e1f6474",
+    "locality": "Hebbala",
+    "areaName": "Sahakar Nagar",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Bakery",
+    "Desserts"
+    ],
+    "avgRating": 4.5,
+    "parentId": "1040",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "100+",
+    "sla": {
+    "deliveryTime": 18,
+    "lastMileTravel": 1.9,
+    "serviceability": "SERVICEABLE",
+    "slaString": "15-20 mins",
+    "lastMileTravelString": "1.9 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-01-28 23:00:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "v1705582451/Ratnesh_Badges/Listing_HR.png",
+    "shortDescription": "Perfect Cake Delivery",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "v1705582451/Ratnesh_Badges/Listing_HR.png",
+    "shortDescription": "Perfect Cake Delivery"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "40% OFF",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "isNewlyOnboarded": true,
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/theobroma-hebbala-sahakar-nagar-bangalore-757051",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {},
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/puliyogare-point-kalpana-chawla-road-hebbal-bangalore-369809",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {},
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/chai-point-brigade-magnum-hebbal-bangalore-200802",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {},
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/freshmenu-kodigehalli-main-road-sahakar-nagar-bangalore-589597",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {},
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/burger-king-rmv-2nd-stage-sanjaynagar-bangalore-8032",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "15384",
+    "name": "Subway",
+    "cloudinaryImageId": "63178e3e64d503a479f2a2048a474552",
+    "locality": "HEBBAL",
+    "areaName": "Esteem Mall",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Salads",
+    "Snacks",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.2,
+    "parentId": "2",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "10K+",
+    "sla": {
+    "deliveryTime": 26,
+    "lastMileTravel": 3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "3.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-01-29 03:00:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹100 OFF",
+    "subHeader": "ABOVE ₹249",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/subway-hebbal-esteem-mall-bangalore-15384",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "584668",
+    "name": "Starbucks Coffee",
+    "cloudinaryImageId": "182191ab163770437b62861a6f987709",
+    "locality": "Rajendra Infotech Road",
+    "areaName": "Sahakar Nagar",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Beverages",
+    "Cafe",
+    "Snacks",
+    "Desserts",
+    "Bakery",
+    "Ice Cream"
+    ],
+    "avgRating": 4.3,
+    "parentId": "195515",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "500+",
+    "sla": {
+    "deliveryTime": 25,
+    "lastMileTravel": 2.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20-25 mins",
+    "lastMileTravelString": "2.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-01-28 23:59:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹100"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-1e5ef0fe-786e-49e7-9e38-02c03b4680a0"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/starbucks-coffee-rajendra-infotech-road-sahakar-nagar-bangalore-584668",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+
+]
+
+
 const RestroCard = (props)  => {
-  console.log(props);
+  // console.log(props); previous example
+  const {restroData} = props;
   return (
     <>
       <div className="Card" style={{backgroundColor: "#414141"}}>
       <img class="sc-dcJsrY kMtDjj" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cace805a6ba74137571d0f7ac92302b1" alt="Chaayos Chai+Snacks=Relax" height= "90px"  style={{borderRadius: "10px"}} />
-      <h3 style={{backgroundColor: "#414141", color: "white" }}>{props.restraunt}</h3>
-      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{props.time}</h3>
-      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{props.cusines}</h3>
-      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{props.starrating}</h3>
+      <h3 style={{backgroundColor: "#414141", color: "white" }}>{restroData[0].info.name}</h3>
+      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{restroData[0].info.areaName}</h3>
+      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{restroData[0].info.costForTwo}</h3>
+      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{restroData[0].info.locality}</h3>
+      
       </div>
     </>
   )
 }
 
 
-const RestroCardd = (props)  => {
-  const {restraunt, time, cusines, starrating} = props
-  return (
-    <>
-      <div className="Card" style={{backgroundColor: "#414141"}}>
-      <img class="sc-dcJsrY kMtDjj" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cace805a6ba74137571d0f7ac92302b1" alt="Chaayos Chai+Snacks=Relax" height= "90px"  style={{borderRadius: "10px"}} />
-      <h3 style={{backgroundColor: "#414141", color: "white" }}>{restraunt}</h3>
-      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{time}</h3>
-      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{cusines}</h3>
-      <h3 style={{backgroundColor: "#414141" , color: "white"}}>{starrating}</h3>
-      </div>
-    </>
-  )
-}
+// const RestroCardd = (props)  => {
+//   const {restraunt, time, cusines, starrating} = props
+//   return (
+//     <>
+//       <div className="Card" style={{backgroundColor: "#414141"}}>
+//       <img class="sc-dcJsrY kMtDjj" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/cace805a6ba74137571d0f7ac92302b1" alt="Chaayos Chai+Snacks=Relax" height= "90px"  style={{borderRadius: "10px"}} />
+//       <h3 style={{backgroundColor: "#414141", color: "white" }}>{restraunt}</h3>
+//       <h3 style={{backgroundColor: "#414141" , color: "white"}}>{time}</h3>
+//       <h3 style={{backgroundColor: "#414141" , color: "white"}}>{cusines}</h3>
+//       <h3 style={{backgroundColor: "#414141" , color: "white"}}>{starrating}</h3>
+//       </div>
+//     </>
+//   )
+// }
 
 
 const Body = function () {
@@ -106,30 +618,15 @@ const Body = function () {
           Search <input type="search" name="search" id="" />
         </div>
       <div className="Flex">
-       <RestroCard  
-          restraunt = "Chaayos Chai+Snacks=Relax"
-          time = "30-35 mins"
-          cusines = "Bakery, Beverages"
-          starrating = "rating 4.3"/>
-       <RestroCard 
-        restraunt = "KFC Chicken"
-        time = "30-35 mins"
-        cusines = "Chicken Berger"
-        starrating = "rating 4.8"/>
+       <RestroCard  restroData = {restaurants}
+       />
       
-       <RestroCardd 
-       restraunt = "DFC Chicken"
-       time = "30-35 mins"
-       cusines = "Chicken Berger"
-       starrating = "rating 4.8"/>
-       <RestroCardd restraunt = "KFC Chicken"
+      
+    
+       {/* <RestroCardd restraunt = "PFC Chicken"
         time = "30-35 mins"
         cusines = "Chicken Berger"
-        starrating = "rating 4.8"/>
-       <RestroCardd restraunt = "PFC Chicken"
-        time = "30-35 mins"
-        cusines = "Chicken Berger"
-        starrating = "rating 4.8"/>
+        starrating = "rating 4.8"/> */}
       
       </div>
       </div>
